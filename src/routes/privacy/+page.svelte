f089d605a6a1e983880aa25b467cab9601920231
RangerDevv <meerabdul2008@gmail.com>
2023-12-30 17:31:50 -0500

    pirvacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,25 @@
+<!-- FILEPATH: /C:/Users/meera/GithubDesktop/AIC-Dismissal/src/routes/privacy/+page.svelte -->
+
+<script>
+    let userLocation = null;
+
+    // Function to collect user location
+    function collectUserLocation() {
+        // Code to collect user location goes here
+        // ...
+    }
+
+    // Function to perform functions based on user location
+    function performFunctionsBasedOnLocation() {
+        // Code to perform functions based on user location goes here
+        // ...
+    }
+</script>
+
+<main>
+    <h1>Privacy Policy</h1>
+    <p>
+        At our website, we collect user location to perform functions pertaining to how close a user is to the mosque.
+        We do not sell any data collected and we ensure that the collected data is 100% secured.
+    </p>
+</main>
