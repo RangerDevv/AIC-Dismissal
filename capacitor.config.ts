50b8815bb871d63815fa45e56ef26a8c7887a51e
RangerDevv <meerabdul2008@gmail.com>
2024-02-06 18:17:34 -0500

    Added capacitor framework for building

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.aicdismissal.vercel',
+  appName: 'AIC-Dismissal',
+  webDir: 'static',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
