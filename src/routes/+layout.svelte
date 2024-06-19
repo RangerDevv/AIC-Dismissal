df24f68679ecfe7d876a95d2c16acb07bbfbdbf3
RangerDevv <meerabdul2008@gmail.com>
2024-06-19 10:12:43 -0400

    Bug fixes and account deletion page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
     import { browser } from "$app/environment";
 
     export let isLoggedIn = false;
+    let loading = true;
     let teacher = false;
 
     let uid = '';
@@ -21,6 +22,7 @@
                 appwriteDatabases.listDocuments(DB_ID,COLLECTION.Parents,[Query.equal('uid',[res['$id']])]).then((res:any) => {
                     teacher = res['documents'][0]['isTeacher'];
                     isLoggedIn = true;
+                    loading = false;
                 }).catch((err) => {
                     console.log(err);
                 });
@@ -29,10 +31,12 @@
         });
         console.log(res);
         isLoggedIn = true;
+        loading = false;
         return res;
         }).catch((err) => {
         console.log(err);
         isLoggedIn = false;
+        loading = false;
         return err;
         });
     }
@@ -66,13 +70,17 @@
         <div class="flex-1">
             <a href="/"><img src="https://www.attawheed.org/uploads/1/1/0/6/110609037/logo-with-text-islamic-center-arabic_1.png" alt="logo" class="h-14 w-auto"/></a>
         </div>
+        {#if loading}
+        <div class="flex-1">
+            <div class="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12"></div>
+        </div>
+        {:else}
         <div class="flex flex-row gap-3">
             {#if !isLoggedIn}
             <a href="/loginRegister/register"><button class="btn btn-primary">Register</button></a>
             <a href="/loginRegister/login"><button class="btn">Login</button></a>
             {/if}
             {#if isLoggedIn}
-            <a href="/dashboard/parentDash"><button class="btn">Home</button></a>
             {#if teacher}
             <a href="/dashboard/teacherDash/"><button class="btn">All Classroom</button></a>
             {#if uid == '659b001f1dd2ea90b3ed' || uid == '658c6e971bcb4f11e387'}
@@ -94,6 +102,7 @@
             <button class="btn btn-primary" on:click={logout}>Logout</button>
             {/if}
         </div>
+        {/if}
     </nav>
     <slot />
 </main>
